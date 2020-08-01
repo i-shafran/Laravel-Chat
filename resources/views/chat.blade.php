4eaa12679353647291e184a9a3a7025f9d26f2e3
Ilya Shafran <ishafran@yandex.ru>
2020-08-01 20:09:38 +0300

    fix chat blade

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <div class="container">
-        <private-chat></private-chat>
+        <div class="col-sm-12">
+            <u>
+                <li><a href="/room/1">The first room</a></li>
+                <li><a href="/room/2">The second room</a></li>
+                <li><a href="/room/3">The last room</a></li>
+            </u>
+        </div>
     </div>
 @endsection
